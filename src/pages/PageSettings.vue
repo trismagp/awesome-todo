dc683c904038471be30f61a3006f3aa3f4705f81
Tristan <tristan@klis-macbook-pro.home>
2020-03-16 13:41:10 +0100

    Add More section in settings page

--- src/pages/PageSettings.vue
+++ src/pages/PageSettings.vue
@@ -19,7 +19,33 @@
             <q-toggle color="blue" v-model="showOneWishList" val="battery" />
           </q-item-section>
         </q-item>
-
+      </q-list>
+      <q-list bordered padding>
+        <q-item-label header>More</q-item-label>
+        <q-item tag="label" v-ripple to="settings/help">
+          <q-item-section>
+            <q-item-label>Help</q-item-label>
+          </q-item-section>
+          <q-item-section side >
+            <q-icon name="chevron_right" />
+          </q-item-section>
+        </q-item>
+        <q-item tag="label" v-ripple @click="visitOurWebSite">
+          <q-item-section>
+            <q-item-label>Visit our webpage</q-item-label>
+          </q-item-section>
+          <q-item-section side >
+            <q-icon name="chevron_right" />
+          </q-item-section>
+        </q-item>
+        <q-item tag="label" v-ripple @click="emailUs">
+          <q-item-section>
+            <q-item-label>Email us</q-item-label>
+          </q-item-section>
+          <q-item-section side >
+            <q-icon name="chevron_right" />
+          </q-item-section>
+        </q-item>
       </q-list>
     </div>
   </q-page>
@@ -27,6 +53,7 @@
 
 <script>
 import { mapState, mapActions } from 'vuex'
+import { openURL } from 'quasar'
 
 export default {
   data(){
@@ -53,7 +80,13 @@ export default {
       }
   },
   methods:{
-        ...mapActions('settings', ['setShow12HourFormat','setShowOneWishList'])
+        ...mapActions('settings', ['setShow12HourFormat','setShowOneWishList']),
+        visitOurWebSite(){          
+          openURL('http://google.com')
+        },
+        emailUs(){
+          window.location.href = 'mailto:tris.magp@gmail.com?subject=Great job'
+        }
   }
 }
 </script>
--- src/router/routes.js
+++ src/router/routes.js
@@ -5,7 +5,8 @@ const routes = [
     component: () => import('layouts/Layout.vue'),
     children: [
       { path: '', component: () => import('pages/PageWishes.vue') },
-      { path: '/settings', component: () => import('pages/PageSettings.vue') }
+      { path: '/settings', component: () => import('pages/PageSettings.vue') },
+      { path: '/settings/help', component: () => import('pages/PageHelp.vue') }
     ]
   }
 ]
